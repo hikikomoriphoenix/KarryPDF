3abffe598793cbc12e5929a7849a836e860c970c
Loremar Marabillas <dondon1987@yandex.com>
2019-03-06 11:11:44 +0800

    Rename PDFObject to IndirectObject

--- app/src/main/java/marabillas/loremar/pdfparser/objects/StreamObject.kt
+++ app/src/main/java/marabillas/loremar/pdfparser/objects/StreamObject.kt
@@ -2,6 +2,23 @@ package marabillas.loremar.pdfparser.objects
 
 import java.io.RandomAccessFile
 
-open class StreamObject(private val file: RandomAccessFile, private val start: Long) : PDFObject(file, start) {
-    val dictionary = Dictionary(file, start)
+open class StreamObject(file: RandomAccessFile, start: Long) : IndirectObject(file, start) {
+    val dictionary = Dictionary(file, start).parse()
+    var streamData: String = ""
+        private set
+
+    init {
+        file.seek(start)
+        var s = ""
+        while (!s.equals("stream", true))
+            s = file.readLine()
+
+        val sb = StringBuilder()
+        while (true) {
+            val l = file.readLine()
+            if (l.equals("endstream", true)) break
+            sb.append(l)
+        }
+        streamData = sb.toString()
+    }
 }

d88386b11e0728b4e88ae11c1ca563c9981d8ccd
Loremar Marabillas <dondon1987@yandex.com>
2019-03-11 04:06:28 +0800

    XRefStream: Choose Channel and ByteBuffer to correctly parse stream data

--- app/src/main/java/marabillas/loremar/pdfparser/XRefStream.kt
+++ app/src/main/java/marabillas/loremar/pdfparser/XRefStream.kt
@@ -5,6 +5,9 @@ import marabillas.loremar.pdfparser.objects.Numeric
 import marabillas.loremar.pdfparser.objects.Stream
 import java.io.RandomAccessFile
 import java.math.BigInteger
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.channels.Channels
 
 /**
  * Class for parsing a cross reference stream
@@ -25,27 +28,34 @@ class XRefStream(private val file: RandomAccessFile, private val start: Long) : 
         var entries = HashMap<String, XRefEntry>()
         if (index != null) {
             println("Parsing XRefStream start")
+            val channel = Channels.newChannel(stream.inputStream())
             for (i in 0 until index.count() step 2) {
                 val start = (index[i] as Numeric).value.toInt()
                 val count = (index[i + 1] as Numeric).value.toInt()
 
                 repeat(count) {
                     print("Parsing XRef entry for obj ${start + it}")
-                    val fields = arrayOf(ByteArray(w0), ByteArray(w1), ByteArray(w2))
-                    repeat(3) { i ->
-                        repeat(fields[i].count()) { j ->
-                            fields[i][j] = stream.inputStream().read().toByte()
-                        }
+                    val fields = arrayOf(ByteBuffer.allocate(w0), ByteBuffer.allocate(w1), ByteBuffer.allocate(w2))
+                    repeat(3) { m ->
+                        fields[m].order(ByteOrder.BIG_ENDIAN)
+                        channel.read(fields[m])
                     }
 
+                    //print(" x=${BigInteger(fields[0])} y=${BigInteger(fields[1])} z=${BigInteger(fields[2])}")
                     val entry = XRefEntry(start + it)
-                    val type = if (fields[0].count() != 0)
-                        BigInteger(fields[0]).toInt()
-                    else 1
+                    val type = if (fields[0].capacity() != 0) {
+                        val bArr = ByteArray(fields[0].capacity())
+                        fields[0].flip()
+                        fields[0].get(bArr)
+                        BigInteger(bArr).toInt()
+                    } else 1
 
-                    val second = if (fields[1].count() != 0)
-                        BigInteger(fields[1]).toInt()
-                    else 0
+                    val second = if (fields[1].capacity() != 0) {
+                        val bArr = ByteArray(fields[1].capacity())
+                        fields[1].flip()
+                        fields[1].get(bArr)
+                        BigInteger(bArr).toLong()
+                    } else 0
 
                     val thirdDefault = when (type) {
                         0 -> 65535
@@ -54,9 +64,12 @@ class XRefStream(private val file: RandomAccessFile, private val start: Long) : 
                         else -> 0
                     }
 
-                    val third = if (fields[2].count() != 0)
-                        BigInteger(fields[2]).toInt()
-                    else thirdDefault
+                    val third = if (fields[2].capacity() != 0) {
+                        val bArr = ByteArray(fields[2].capacity())
+                        fields[2].flip()
+                        fields[2].get(bArr)
+                        BigInteger(bArr).toInt()
+                    } else thirdDefault
 
                     when (type) {
                         0 -> {
@@ -64,13 +77,13 @@ class XRefStream(private val file: RandomAccessFile, private val start: Long) : 
                             entry.inUse = false
                         }
                         1 -> {
-                            entry.pos = second.toLong()
+                            entry.pos = second
                             entry.gen = third
                         }
                         2 -> {
                             entry.gen = 0
                             entry.compressed = true
-                            entry.objStm = second
+                            entry.objStm = second.toInt()
                             entry.index = third
                         }
                         else -> {
@@ -83,6 +96,7 @@ class XRefStream(private val file: RandomAccessFile, private val start: Long) : 
                 }
             }
             println("Parsing XRefStream end")
+            channel.close()
         }
 
         val prev = dictionary["Prev"] as Numeric?

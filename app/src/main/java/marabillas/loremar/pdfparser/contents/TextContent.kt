6902916dc1face3d02536f7b0c4a19a229209d98
Loremar Marabillas <dondon1987@yandex.com>
2019-03-21 02:34:11 +0800

    Make TextContent's constructor internal

--- app/src/main/java/marabillas/loremar/pdfparser/contents/TextContent.kt
+++ app/src/main/java/marabillas/loremar/pdfparser/contents/TextContent.kt
@@ -1,6 +1,6 @@
 package marabillas.loremar.pdfparser.contents
 
-data class TextContent(
+data class TextContent internal constructor(
     val tj: String = "",
     val tf: String = "",
     val td: String = "",

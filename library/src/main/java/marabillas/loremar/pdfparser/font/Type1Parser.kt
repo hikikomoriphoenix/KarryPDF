a825b60533e09970b6644799943f78cba196bf30
Loremar Marabillas <dondon1987@yandex.com>
2019-06-01 03:02:51 +0800

    Get character widths with built-in encoding as base encoding

--- library/src/main/java/marabillas/loremar/pdfparser/font/Type1Parser.kt
+++ library/src/main/java/marabillas/loremar/pdfparser/font/Type1Parser.kt
@@ -1,6 +1,7 @@
 package marabillas.loremar.pdfparser.font
 
 import android.support.v4.util.SparseArrayCompat
+import marabillas.loremar.pdfparser.toInt
 import java.io.ByteArrayOutputStream
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
@@ -323,7 +324,7 @@ internal class Type1Parser(val data: ByteArray) {
         val encodingPos = getEncodingLocation() + 10
         when {
             isStandardEncoding(encodingPos) -> {
-                println("Using StandardEncoding")
+                println("Using StandardEncoding as base encoding")
                 for (i in 0..255) {
                     var charName = diffArray[i]
                     if (charName == null) {
@@ -341,7 +342,10 @@ internal class Type1Parser(val data: ByteArray) {
             isMacRomanEncoding(encodingPos) -> TODO("Needs to use MacRomanEncoding to get widths")
             isMacExpertEncoding(encodingPos) -> TODO("Needs to use MacExpertEncoding to get widths")
             isWinAnsiEncoding(encodingPos) -> TODO("Needs to use WinAnsiEncoding to get widths")
-            else -> TODO("Needs to use built-in Encoding to get Widths")
+            else -> {
+                println("Using built-in encoding as base encoding")
+                useBuiltInEncodingToGetWidths(encodingPos, characterWidths, diffArray)
+            }
         }
 
         // Assign width for missing character
@@ -598,6 +602,66 @@ internal class Type1Parser(val data: ByteArray) {
             standard[245] = "yen"
             standard[172] = "z"
             standard[160] = "zero"
+        }
+    }
+
+    private fun useBuiltInEncodingToGetWidths(
+        start: Int,
+        characterWidths: SparseArrayCompat<Float>,
+        diffArray: SparseArrayCompat<String>
+    ) {
+        var i = start
+        while (i + 2 < data.size) {
+            if (data[i].toChar() == 'p' && data[i + 1].toChar() == 'u' && data[i + 2].toChar() == 't') {
+                // Locate character name beginning
+                var p = i
+                while (data[p].toChar() != '/')
+                    p--
+
+                // Extract and save character name
+                var c = 1
+                stringBuilder.clear()
+                while (data[p + c].toChar() != ' ')
+                    stringBuilder.append(data[p + c++])
+                val charName = stringBuilder.toString()
+
+                // Locate character code beginning
+                p -= 2 // Skip space
+                while (data[p].toChar() != ' ')
+                    p--
+
+                // Extract and save character code
+                c = 1
+                stringBuilder.clear()
+                while (data[p + c].toChar() != ' ')
+                    stringBuilder.append(data[p + c++])
+                val charCode = stringBuilder.toInt()
+
+                // Get width and add to characterWidths with charCode as key
+                val command = charCommands[charName]
+                val hsbw = command?.get(CharCommand.HSBW)
+                val width = hsbw?.get(1)
+                width?.let {
+                    characterWidths[charCode] = width.toFloat()
+                }
+
+                // Move to end of 'put' operator
+                i += 2
+            } else if (data[i].toChar() == 'd' && data[i + 1].toChar() == 'e' && data[i + 2].toChar() == 'f') {
+                break
+            }
+            i++
+        }
+
+        // Override with diffArray
+        for (k in 0 until diffArray.size()) {
+            val charName = diffArray.valueAt(k)
+            val command = charCommands[charName]
+            val hsbw = command?.get(CharCommand.HSBW)
+            val width = hsbw?.get(1)
+            width?.let {
+                characterWidths[k] = width.toFloat()
+            }
         }
     }
 }

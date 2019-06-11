baca4b70acba63e02d689b41f4d3043615682011
Loremar Marabillas <dondon1987@yandex.com>
2019-06-12 00:00:01 +0800

    Refactor Font Class

--- library/src/main/java/marabillas/loremar/pdfparser/font/Font.kt
+++ library/src/main/java/marabillas/loremar/pdfparser/font/Font.kt
@@ -21,35 +21,49 @@ internal class Font() {
     constructor(dictionary: Dictionary, referenceResolver: ReferenceResolver) : this() {
         dictionary.resolveReferences()
         val fontDescriptor = dictionary["FontDescriptor"]
+        // Get type of font
+        val subtype = dictionary["Subtype"]
+        // Get name of font
+        val baseFont = dictionary["BaseFont"]
+        // Get encoding used for font
+        val encoding = dictionary["Encoding"]
+        // Get cmap of character code to unicode mappings
+        val toUnicode = dictionary["ToUnicode"]
+
+        processDictionaryEntries(fontDescriptor, subtype, baseFont, encoding, toUnicode, referenceResolver)
+
+        if (subtype is Name && subtype.value != "Type0") {
+            setupSimpleFont(dictionary, fontDescriptor, subtype, baseFont, encoding, referenceResolver)
+        } else {
+            setupCompositeFont(dictionary, encoding, referenceResolver)
+        }
+    }
+
+    private fun processDictionaryEntries(
+        fontDescriptor: PDFObject?,
+        subtype: PDFObject?,
+        baseFont: PDFObject?,
+        encoding: PDFObject?,
+        toUnicode: PDFObject?,
+        referenceResolver: ReferenceResolver
+    ) {
+        // FontDescriptor
         if (fontDescriptor is Dictionary) {
             fontDescriptor.resolveReferences()
         }
 
-        // Get type of font
-        val subtype = dictionary["Subtype"]
+        // Subtype
         if (subtype is Name) {
             println("Using ${subtype.value} font")
         }
 
-        // Get typeface
-        val baseFont = dictionary["BaseFont"]
+        // BaseFont
         if (baseFont is Name) {
             typeface = FontIdentifier().identifyFont(baseFont.value)
             println("Basefont=${baseFont.value}")
         }
 
-        // Get cmap
-        val toUnicode = dictionary["ToUnicode"]
-        if (toUnicode is Reference) {
-            val stream = referenceResolver.resolveReferenceToStream(toUnicode)
-            val b = stream?.decodeEncodedStream()
-            b?.let {
-                cmap = ToUnicodeCMap(String(b)).parse()
-            }
-        }
-
-        // Get Encoding
-        val encoding = dictionary["Encoding"]
+        // Encoding
         if (encoding is Name) {
             println("Font uses ${encoding.value}")
         } else if (encoding is Dictionary) {
@@ -58,186 +72,207 @@ internal class Font() {
             println("Font is a Composite Font and may be using a CMap")
         }
 
-        // If a Simple Font
-        if (subtype is Name && subtype.value != "Type0") {
-            if (cmap !is ToUnicodeCMap) {
-                val encodingArray = SparseArrayCompat<String>()
-                var symbolic = false
+        // ToUnicode
+        if (toUnicode is Reference) {
+            val stream = referenceResolver.resolveReferenceToStream(toUnicode)
+            val b = stream?.decodeEncodedStream()
+            b?.let {
+                cmap = ToUnicodeCMap(String(b)).parse()
+            }
+        }
+    }
 
-                // Check if font is symbolic
-                if (fontDescriptor is Dictionary) {
-                    val flags = (fontDescriptor["Flags"] as Numeric).value.toInt()
-                    // Check if third lower bit is set as 1 in flags(equals to 4 if only this flag is set). If it is,
-                    // then font is symbolic.
-                    if ((flags and 4) != 0) {
-                        symbolic = true
-                        println("Symbolic flag is set true")
-                        if (baseFont is Name && baseFont.value.contains("Zapf", true)
-                            && baseFont.value.contains("Dingbats", true)
-                        ) {
-                            ZapfDingbatsEncoding.putAllTo(encodingArray)
-                        } else if (baseFont is Name && baseFont.value.contains("Symbol", true)) {
-                            SymbolEncoding.putAllTo(encodingArray)
-                        }
-                    }
-                }
+    private fun setupSimpleFont(
+        dictionary: Dictionary,
+        fontDescriptor: PDFObject?,
+        subtype: Name,
+        baseFont: PDFObject?,
+        encoding: PDFObject?,
+        referenceResolver: ReferenceResolver
+    ) {
+        if (cmap !is ToUnicodeCMap) {
+            val encodingArray = SparseArrayCompat<String>()
 
-                cmap = when (encoding) {
-                    is Name -> {
-                        when (encoding.value) {
-                            "MacRomanEncoding" -> MacRomanEncoding.putAllTo(encodingArray)
-                            "WinAnsiEncoding" -> WinAnsiEncoding.putAllTo(encodingArray)
-                            "MacExpertEncoding" -> MacExpertEncoding.putAllTo(encodingArray)
-                            "StandardEncoding" -> StandardEncoding.putAllTo(encodingArray)
-                        }
+            val symbolic = checkSymbolicSimpleFont(fontDescriptor, baseFont, encodingArray)
 
-                        // If symbolic, get encoding from embedded font program to get symbolic characters
-                        if (symbolic && fontDescriptor is Dictionary) {
-                            getEncodingFromBuiltInProgram(
-                                fontDescriptor,
-                                referenceResolver,
-                                encodingArray,
-                                subtype
-                            )
-                        }
+            cmap = getSimpleFontEncoding(encoding, encodingArray, fontDescriptor, subtype, referenceResolver, symbolic)
+        }
 
-                        AGLCMap(encodingArray)
-                    }
-                    is Dictionary -> {
-                        val baseEncoding = encoding["BaseEncoding"]
-                        if (baseEncoding is Name) {
-                            when (baseEncoding.value) {
-                                "MacRomanEncoding" -> MacRomanEncoding.putAllTo(encodingArray)
-                                "WinAnsiEncoding" -> WinAnsiEncoding.putAllTo(encodingArray)
-                                "MacExpertEncoding" -> MacExpertEncoding.putAllTo(encodingArray)
-                                "StandardEncoding" -> StandardEncoding.putAllTo(encodingArray)
-                            }
+        getSimpleFontWidths(dictionary, fontDescriptor, referenceResolver, encoding, subtype)
+    }
 
-                            // If symbolic, get encoding from embedded font program to get symbolic characters
-                            if (symbolic && fontDescriptor is Dictionary) {
-                                getEncodingFromBuiltInProgram(
-                                    fontDescriptor,
-                                    referenceResolver,
-                                    encodingArray,
-                                    subtype
-                                )
-                            }
-                        } else {
-                            // Get encoding from embedded font program.
-                            if (fontDescriptor is Dictionary) {
-                                getEncodingFromBuiltInProgram(
-                                    fontDescriptor,
-                                    referenceResolver,
-                                    encodingArray,
-                                    subtype
-                                )
-                            } else if (!symbolic) {
-                                StandardEncoding.putAllTo(encodingArray)
-                            }
-                        }
-                        getDifferencesArray(encoding, encodingArray)
-                        AGLCMap(encodingArray)
-                    }
-                    else -> {
-                        // Get character names from embedded font program. Get unicode from AGL
-                        if (fontDescriptor is Dictionary) {
-                            getEncodingFromBuiltInProgram(fontDescriptor, referenceResolver, encodingArray, subtype)
-                        } else if (!symbolic) {
-                            StandardEncoding.putAllTo(encodingArray)
-                        }
-                        AGLCMap(encodingArray)
-                    }
+    private fun checkSymbolicSimpleFont(
+        fontDescriptor: PDFObject?,
+        baseFont: PDFObject?,
+        encodingArray: SparseArrayCompat<String>
+    ): Boolean {
+        var symbolic = false
+        if (fontDescriptor is Dictionary) {
+            val flags = (fontDescriptor["Flags"] as Numeric).value.toInt()
+            // Check if third lower bit is set as 1 in flags(equals to 4 if only this flag is set). If it is,
+            // then font is symbolic.
+            if ((flags and 4) != 0) {
+                symbolic = true
+                println("Symbolic flag is set true")
+                if (baseFont is Name && baseFont.value.contains("Zapf", true)
+                    && baseFont.value.contains("Dingbats", true)
+                ) {
+                    ZapfDingbatsEncoding.putAllTo(encodingArray)
+                } else if (baseFont is Name && baseFont.value.contains("Symbol", true)) {
+                    SymbolEncoding.putAllTo(encodingArray)
                 }
             }
+        }
+        return symbolic
+    }
 
-            // Get FirstChar
-            var firstChar = 0
-            val fc = dictionary["FirstChar"]
-            if (fc is Numeric) {
-                firstChar = fc.value.toInt()
+    private fun getSimpleFontEncoding(
+        encoding: PDFObject?,
+        encodingArray: SparseArrayCompat<String>,
+        fontDescriptor: PDFObject?,
+        subtype: Name,
+        referenceResolver: ReferenceResolver,
+        symbolic: Boolean
+    ): CMap {
+        return when (encoding) {
+            is Name -> {
+                handleSimpleFontPredefinedEncoding(encoding, encodingArray)
+                // If symbolic, get encoding from embedded font program to get symbolic characters
+                if (symbolic && fontDescriptor is Dictionary)
+                    getEncodingFromBuiltInProgram(fontDescriptor, referenceResolver, encodingArray, subtype)
+                AGLCMap(encodingArray)
             }
-
-            // Get MissingWidth
-            var missingWidth: Float? = null
-            if (fontDescriptor is Dictionary) {
-                val mw = fontDescriptor["MissingWidth"]
-                if (mw is Numeric) {
-                    missingWidth = mw.value.toFloat()
+            is Dictionary -> {
+                val baseEncoding = encoding["BaseEncoding"]
+                if (baseEncoding is Name) {
+                    handleSimpleFontPredefinedEncoding(baseEncoding, encodingArray)
+                    // If symbolic, get encoding from embedded font program to get symbolic characters
+                    if (symbolic && fontDescriptor is Dictionary) {
+                        getEncodingFromBuiltInProgram(fontDescriptor, referenceResolver, encodingArray, subtype)
+                    }
+                } else {
+                    // Get encoding from embedded font program.
+                    if (fontDescriptor is Dictionary)
+                        getEncodingFromBuiltInProgram(fontDescriptor, referenceResolver, encodingArray, subtype)
+                    else if (!symbolic)
+                        StandardEncoding.putAllTo(encodingArray)
                 }
+                getDifferencesArray(encoding, encodingArray)
+                AGLCMap(encodingArray)
             }
+            else -> {
+                // Get character names from embedded font program. Get unicode from AGL
+                if (fontDescriptor is Dictionary)
+                    getEncodingFromBuiltInProgram(fontDescriptor, referenceResolver, encodingArray, subtype)
+                else if (!symbolic)
+                    StandardEncoding.putAllTo(encodingArray)
+                AGLCMap(encodingArray)
+            }
+        }
+    }
 
-            // Get widths
-            val ws = dictionary["Widths"]
-            if (ws is PDFArray && missingWidth != null) {
-                // Assign missing character width
-                widths.put(-1, missingWidth)
+    private fun handleSimpleFontPredefinedEncoding(encoding: Name, encodingArray: SparseArrayCompat<String>) {
+        when (encoding.value) {
+            "MacRomanEncoding" -> MacRomanEncoding.putAllTo(encodingArray)
+            "WinAnsiEncoding" -> WinAnsiEncoding.putAllTo(encodingArray)
+            "MacExpertEncoding" -> MacExpertEncoding.putAllTo(encodingArray)
+            "StandardEncoding" -> StandardEncoding.putAllTo(encodingArray)
+        }
+    }
 
-                for (i in 0 until ws.count()) {
-                    widths.put(firstChar + i, (ws[i] as Numeric).value.toFloat())
-                }
-            } else {
-                if (fontDescriptor is Dictionary) {
-                    getWidthsFromFontProgram(fontDescriptor, referenceResolver, encoding, subtype)
-                }
+    private fun getSimpleFontWidths(
+        dictionary: Dictionary,
+        fontDescriptor: PDFObject?,
+        referenceResolver: ReferenceResolver,
+        encoding: PDFObject?, subtype: Name
+    ) {
+        // Get FirstChar
+        var firstChar = 0
+        val fc = dictionary["FirstChar"]
+        if (fc is Numeric) {
+            firstChar = fc.value.toInt()
+        }
+
+        // Get MissingWidth
+        var missingWidth: Float? = null
+        if (fontDescriptor is Dictionary) {
+            val mw = fontDescriptor["MissingWidth"]
+            if (mw is Numeric) {
+                missingWidth = mw.value.toFloat()
+            }
+        }
+
+        // Get widths
+        val ws = dictionary["Widths"]
+        if (ws is PDFArray && missingWidth != null) {
+            // Assign missing character width
+            widths.put(-1, missingWidth)
+
+            for (i in 0 until ws.count()) {
+                widths.put(firstChar + i, (ws[i] as Numeric).value.toFloat())
             }
         } else {
-            // Else a Composite Font
-
-            // Handle encoding
-            if (encoding is Name && !encoding.value.contains("Identity")) {
-                TODO("Needs to handle predefined CMaps. If ToUnicodeCMap exist, pass it to constructor.")
-            } else if (encoding is Reference) {
-                TODO("Needs to handle embedded CMaps. If ToUnicodeCMap exist, pass it to constructor.")
+            if (fontDescriptor is Dictionary) {
+                getWidthsFromFontProgram(fontDescriptor, referenceResolver, encoding, subtype)
             }
+        }
+    }
 
-            // Get widths and missing characters width
-            val descendant = dictionary["DescendantFonts"]
-            if (descendant is PDFArray) {
-                descendant.resolveReferences()
-                val cidFont = descendant[0]
-                if (cidFont is Dictionary) {
-                    cidFont.resolveReferences()
+    private fun setupCompositeFont(dictionary: Dictionary, encoding: PDFObject?, referenceResolver: ReferenceResolver) {
+        // Handle encoding
+        if (encoding is Name && !encoding.value.contains("Identity")) {
+            TODO("Needs to handle predefined CMaps. If ToUnicodeCMap exist, pass it to constructor.")
+        } else if (encoding is Reference) {
+            TODO("Needs to handle embedded CMaps. If ToUnicodeCMap exist, pass it to constructor.")
+        }
 
-                    // Get default width
-                    val dw = cidFont["DW"]
-                    if (dw is Numeric) {
-                        widths.put(-1, dw.value.toFloat())
-                    } else {
-                        widths.put(-1, 1000f)
-                    }
+        // Get widths and missing characters width
+        val descendant = dictionary["DescendantFonts"]
+        if (descendant is PDFArray) {
+            descendant.resolveReferences()
+            val cidFont = descendant[0]
+            if (cidFont is Dictionary) {
+                cidFont.resolveReferences()
 
-                    // Get MissingWidth
-                    val cidFontDescriptor = cidFont["FontDescriptor"]
-                    if (cidFontDescriptor is Dictionary) {
-                        cidFontDescriptor.resolveReferences()
-                        val mw = cidFontDescriptor["MissingWidth"]
-                        if (mw is Numeric) {
-                            widths.put(-1, mw.value.toFloat())
-                        }
-                    }
-
-                    if (widths[-1] == 0f && cidFontDescriptor is Dictionary) {
-                        cidFontDescriptor.resolveReferences()
-                        val cidSubType = cidFont["Subtype"]
-                        if (cidSubType is Name && cidSubType.value == "CIDFontType2") {
-                            println("Getting widths for CIDFontType2")
-                            getCIDType2Widths(cidFont, cidFontDescriptor, referenceResolver)
-                            println("obtained ${widths.size()} widths")
-                        } else if (cidSubType is Name) {
-                            // Subtype is CIDFontType0. Use FontFile3 with subtype either CIDFontType0C or OpenType
-                            getWidthsFromFontProgram(cidFontDescriptor, referenceResolver, encoding, cidSubType)
-                        }
-                    } else {
-                        // Get widths
-                        val w = cidFont["W"]
-                        if (w is PDFArray) {
-                            getCIDFontWidths(w)
-                            println("obtained ${widths.size()} widths")
-                        }
-                    }
-
-                    println("missing width = ${widths[-1]}")
+                // Get default width
+                val dw = cidFont["DW"]
+                if (dw is Numeric) {
+                    widths.put(-1, dw.value.toFloat())
+                } else {
+                    widths.put(-1, 1000f)
                 }
+
+                // Get MissingWidth
+                val cidFontDescriptor = cidFont["FontDescriptor"]
+                if (cidFontDescriptor is Dictionary) {
+                    cidFontDescriptor.resolveReferences()
+                    val mw = cidFontDescriptor["MissingWidth"]
+                    if (mw is Numeric) {
+                        widths.put(-1, mw.value.toFloat())
+                    }
+                }
+
+                if (widths[-1] == 0f && cidFontDescriptor is Dictionary) {
+                    cidFontDescriptor.resolveReferences()
+                    val cidSubType = cidFont["Subtype"]
+                    if (cidSubType is Name && cidSubType.value == "CIDFontType2") {
+                        println("Getting widths for CIDFontType2")
+                        getCIDType2Widths(cidFont, cidFontDescriptor, referenceResolver)
+                        println("obtained ${widths.size()} widths")
+                    } else if (cidSubType is Name) {
+                        // Subtype is CIDFontType0. Use FontFile3 with subtype either CIDFontType0C or OpenType
+                        getWidthsFromFontProgram(cidFontDescriptor, referenceResolver, encoding, cidSubType)
+                    }
+                } else {
+                    // Get widths
+                    val w = cidFont["W"]
+                    if (w is PDFArray) {
+                        getCIDFontWidths(w)
+                        println("obtained ${widths.size()} widths")
+                    }
+                }
+
+                println("missing width = ${widths[-1]}")
             }
         }
     }

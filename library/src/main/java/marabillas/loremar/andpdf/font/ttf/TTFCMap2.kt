80ac3f926bcdcf58f0e762eb20b93d6ca74bbc48
Loremar Marabillas <dondon1987@yandex.com>
2019-06-20 15:38:07 +0800

    Add support for TrueType cmap format 2

--- library/src/main/java/marabillas/loremar/andpdf/font/ttf/TTFCMap2.kt
+++ library/src/main/java/marabillas/loremar/andpdf/font/ttf/TTFCMap2.kt
@@ -1,12 +1,40 @@
 package marabillas.loremar.andpdf.font.ttf
 
-import marabillas.loremar.andpdf.exceptions.UnsupportedPDFElementException
+import marabillas.loremar.andpdf.utils.exts.set
 
 internal class TTFCMap2(data: ByteArray, pos: Long) : TTFCMapDefault() {
     init {
-        throw UnsupportedPDFElementException(
-            "TrueType fonts with Chinese, Korean, or Japanese characters may not be " +
-                    "supported yet"
-        )
+        var hiBytePos = pos + 6
+        val subHeaderStart = hiBytePos + (256 * 2)
+        repeat(256) { hiByte ->
+            val k = TTFParser.getUInt16At(data, hiBytePos.toInt()) / 8
+            if (k == 0) {
+                val idRangeOffsetPos = subHeaderStart.toInt() + 6
+                val idRangeOffset = TTFParser.getUInt16At(data, idRangeOffsetPos)
+                val firstCodePos = idRangeOffsetPos + idRangeOffset
+                val glyphIndexPos = firstCodePos + (hiByte * 2)
+                val glyphIndex = TTFParser.getUInt16At(data, glyphIndexPos)
+                map[hiByte] = glyphIndex
+            } else {
+                val subHeaderPos = subHeaderStart.toInt() + (k * 8)
+                val firstCode = TTFParser.getUInt16At(data, subHeaderPos)
+                val entryCount = TTFParser.getUInt16At(data, subHeaderPos + 2)
+                val idDelta = TTFParser.getUInt16At(data, subHeaderPos + 4)
+                val idRangeOffset = TTFParser.getUInt16At(data, subHeaderPos + 6)
+                val idRangeOffsetPos = subHeaderPos + 6
+                val firstCodePos = idRangeOffsetPos + idRangeOffset
+                repeat(entryCount) { j ->
+                    val secondByte = firstCode + j
+                    val glyphIndexPos = firstCodePos + (j * 2)
+                    var glyphIndex = TTFParser.getUInt16At(data, glyphIndexPos)
+                    if (glyphIndex != 0) {
+                        glyphIndex += idDelta
+                    }
+                    val charCode = (hiByte shl 8) or secondByte
+                    map[charCode] = glyphIndex
+                }
+            }
+            hiBytePos += 2
+        }
     }
 }

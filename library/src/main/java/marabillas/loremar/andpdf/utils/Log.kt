7a0c9f6b7f5d5a92997e16c321c38458d51cc179
Loremar Marabillas <dondon1987@yandex.com>
2019-07-12 02:36:28 +0800

    Replace Log with System's print function

--- library/src/main/java/marabillas/loremar/andpdf/utils/Log.kt
+++ library/src/main/java/marabillas/loremar/andpdf/utils/Log.kt
@@ -1,18 +1,16 @@
 package marabillas.loremar.andpdf.utils
 
-import android.util.Log
-
 internal var showAndPDFLogs = false
-internal const val andPDFLogsTag = "AndPDFLogs"
 
 internal fun logd(message: String) {
     if (showAndPDFLogs) {
-        Log.d(andPDFLogsTag, message)
+        println(message)
     }
 }
 
 internal fun loge(message: String, e: Exception? = null) {
     if (showAndPDFLogs) {
-        Log.e(andPDFLogsTag, message, e)
+        System.err.println(message)
+        e?.printStackTrace()
     }
 }
